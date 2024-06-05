bcc4049ae61ee94e1a1921af315e58440cbf46e6
sweta930 <142826545+sweta930@users.noreply.github.com>
2024-06-05 13:24:06 +0000

    datatype summary

---
+++ 01-basics/01-datatype.js
@@ -0,0 +1,33 @@
+// let num = 32;
+// let stringnum = String(num);
+// console.log(typeof stringnum);
+// console.log(stringnum);
+
+// let myNum = ("2"===2)
+// console.log(myNum);
+
+let num1 = 12;
+let num2 =12.4;
+let userName = "sweta";
+const weather = null;
+const islogedIn = true;
+let emailId;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+const bigNum = 1398479689255885437097374n;
+
+let obj = {
+    name: "sweta",
+    userId: 123,
+    age:22
+};
+
+const arr = ["ram","shyam","lakhan"];
+
+const myFunc = function(){
+    console.log("hello world");
+}
+
+console.log(typeof weather);
